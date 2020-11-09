d7e1cdf7d3c1a1cdf9b71e3dbdd9972e9450ba6f
primuppa <45830626+primuppa@users.noreply.github.com>
2020-11-09 11:40:04 -0500

    Create rock-paper-scissors.py

---
+++ rock-paper-scissors.py
@@ -0,0 +1,7 @@
+choice = ["Rock", "Paper", "Scissors"] computer = random.choice(choice)
+
+player = False
+
+while player == False: print("Welcome to Rock, Paper and Scissors!") print("\nPlease, wait the game is loading....") sleep(10) player = input("Which one do you want to choose?\n'Rock': 'Rock'\n'Paper': 'Paper'\n'Scissors': 'Scissors'\n'Stop the game': 'Stop': ") if player == computer: print("\nPlease wait we are loading your results...") sleep(2) print("It's a Tie!") elif player == "Rock": if computer == "Paper": print("\nPlease wait we are loading your results...") sleep(2) print("You Have Lost!!") else: print("\nPlease wait we are loading your results...") sleep(2) print("You Have Won!!") elif player == "Scissors": if computer == "Rock": print("\nPlease wait we are loading your results...") sleep(2) print("You Have Lost!!") else: print("\nPlease wait we are loading your results...") sleep(2) print("You Have Won!!") elif player == "Paper": if computer == "Scissors": print("\nPlease wait we are loading your results...") sleep(2) print("You have Lost!!") else: print("\nPlease wait we are loading your results...") sleep(2) print("You have won!!") elif player == "Stop": print("Thanks for playing!!") break else: print("That's not a valid play!!") break
+
+player = False
